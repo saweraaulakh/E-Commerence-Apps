be1d48eb8a4dbc955ee7d0f601ceb3fe5ec13272
Noor Fatima <saweraaulakh@gmail.com>
2025-08-22 12:58:22 +0500

    Update vise-required.js

--- (tabs)/vise-required.js
+++ (tabs)/vise-required.js
@@ -1,1 +1,9 @@
+import { View, Text } from 'react-native';
 
+export default function VisaRequiredScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Visa Required Screen</Text>
+    </View>
+  );
+}
